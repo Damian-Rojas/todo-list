70be8b71f08a5afb4e39426a491f12a9c1434307
Damian Rojas <rojasdamianc@gmail.com>
2023-12-21 23:28:51 -0300

    Se crea punto de entrada de la aplicacion run.py y define configuración del proyecto

---
+++ todor/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+def create_app():
+    app=Flask (__name__)
+
+    # cofiguracion del proyecto
+    app.config.from_mapping(
+        DEBUG= True,
+        SECRET_KEY='dev'
+    )
+
+    @app.route('/')
+    def index():
+        return 'Hola mundo'
+        
+    return app
