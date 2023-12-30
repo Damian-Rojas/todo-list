4b47eec0593dcfd89dafc0380b3b93141c6edddb
Damian Rojas <rojasdamianc@gmail.com>
2023-12-29 21:19:13 -0300

    Se agrega pagina de login y registro

--- todor/auth.py
+++ todor/auth.py
@@ -1,12 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp=Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('register/')
 def register():
-    return "Registrar Usuario"
+    return render_template('auth/register.html')
 
 
 @bp.route('login/')
-def create():
-    return "Iniciar sesion"
+def login():
+     return render_template('auth/login.html')
