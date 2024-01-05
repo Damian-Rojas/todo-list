0ab1772ec52f87ab022c642ed07debfa676ce6c6
Damian Rojas <rojasdamianc@gmail.com>
2024-01-04 23:32:44 -0300

    Creacion de base con SQLAlchemy

--- todor/__init__.py
+++ todor/__init__.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+# Crear la extensión
+db=SQLAlchemy()
+
+
 def create_app():
     app=Flask (__name__)
 
     # cofiguracion del proyecto
     app.config.from_mapping(
         DEBUG= True,
-        SECRET_KEY='dev'
+        SECRET_KEY='dev',
+        SQLALCHEMY_DATABASE_URI="sqlite:///todolist.db"
     )
+
+    # Inicializar la extensión 
+    db.init_app(app)
+
+
     # Registrar Bluprint
     from . import todo
     app.register_blueprint(todo.bp)
@@ -18,5 +30,9 @@ def create_app():
     @app.route('/')
     def index():
         return render_template ('index.html')
+    
+    # Migra todos los modelos a la base de datos que no han sido migrados
+    with app.app_context():
+        db.create_all()
         
     return app
