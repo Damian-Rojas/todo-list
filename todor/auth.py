65da8e452c8589a954c70ea3cba38287e5fd1c13
Damian Rojas <rojasdamianc@gmail.com>
2024-02-12 17:37:41 -0300

    Se agrega inicio de sesión del usuario

--- todor/auth.py
+++ todor/auth.py
@@ -60,3 +60,15 @@ def login():
 
 
         return render_template('auth/login.html')
+
+# Función verificar si el usuario que inicia Sesion
+@bp.before_app_request #Ejecuta la función en cada petición
+def load_logged_in_used():
+    user_id=session.get('user_id') #Obtiene el id del usuario que tiene la sesion
+
+    if user_id is None:
+        g.user=None
+    else:
+        g.user=User.query.get_or_404(user_id)
+          
+
