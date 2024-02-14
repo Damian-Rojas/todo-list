fb7e9152e938b545700f5566165eb4cb2d520e7c
Damian Rojas <rojasdamianc@gmail.com>
2024-02-13 23:39:54 -0300

    Se agrega operación ingreso de tarea

--- todor/todo.py
+++ todor/todo.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for,g
+
 from todor.auth import login_required
 from .models import Todo, User
 from todor import db
@@ -15,6 +16,17 @@ def index():
     return render_template('todo/index.html', todos=todos)
 
 
-@bp.route('create/')
+@bp.route('create/', methods=('GET','POST'))
+@login_required
 def create():
-    return "Crear tareas"
+    if request.method == 'POST':
+        title=request.form['title']
+        desc=request.form['desc']
+
+        todo=Todo(g.user.id, title, desc)
+
+        db.session.add(todo)
+        db.session.commit()
+        return redirect(url_for ('todo.index'))
+
+    return render_template ('todo/create.html')
